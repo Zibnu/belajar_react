563610ec68bd02fa173dd0b3f7e87d037d92ffd4
Zibnu <crimhomid@gmail.com>
2025-08-19 20:46:26 +0700

    Membuat navbar

---
+++ my-web/src/App.jsx
@@ -0,0 +1,25 @@
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Navbar />
+      <h1>Welcome to ZibnuStore </h1>
+      <p>Tempat Jual Beli Diamond murah terpercaya</p>
+      <h5>Product</h5>
+      <ol>
+        <li>Diamond MLBB</li>
+        <li>UC PUBG GLOBAL</li>
+        <li>Diamond Free fire</li>
+      </ol>
+    </>
+  );
+}
+
+export default App;
---
+++ my-web/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <span>ZibnuStore</span>
+      </div>
+      <ul className="link">
+        <li>Beranda</li>
+        <li>Product</li>
+        <li>Jelajahi</li>
+        <li>Favorit</li>
+      </ul>
+    </nav>
+  );
+}
+export default Navbar;
